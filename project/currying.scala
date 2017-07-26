a90c40eda4f3992ee947bc76350846220bf5f775
Tarun <tarunk@nr-online.com>
2017-07-26 22:42:11 +0100

    Cookbook project added

--- project/currying.scala
+++ project/currying.scala
@@ -3,10 +3,12 @@ import Keys._
 
 object HelloBuild extends Build {
     lazy val root = Project(id = "currying",
-                            base = file(".")) aggregate(scala_core,spark_core)
+                            base = file(".")) aggregate(scala_core,spark_core,cookbook)
 
     lazy val scala_core = Project(id = "scala_core",
                            base = file("scala"))
     lazy val spark_core = Project(id = "spark_core",
                            base = file("spark"))
+    lazy val cookbook = Project(id = "cookbook",
+                          base = file("cookbook"))
 }
